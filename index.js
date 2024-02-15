28bb3932464b15d9d891eae621ac8f39faf3cf37
SlviACH6 <146087759+SlviACH6@users.noreply.github.com>
2024-02-15 13:20:08 +0700

    create photos route

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 import express from 'express'
 const app = express()
-app.listen(4100, () => { 
-    console.log('Airbnb API ready on localhost:4100')
+
+import photosRouter from './routes/photosRoutes.js'
+
+app.use(photosRouter)
+
+app.listen(4100, () => {
+  console.log('Airbnb API ready on localhost:4100')
 })
