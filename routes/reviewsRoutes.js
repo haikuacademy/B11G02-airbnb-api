f0fad0c88eafdb458af33cf1703806b518a3e8e8
ValeriaSml <114675105+ValeriaSml@users.noreply.github.com>
2024-02-15 13:25:40 +0700

    change send for json

--- routes/reviewsRoutes.js
+++ routes/reviewsRoutes.js
@@ -2,14 +2,14 @@ import { Router } from 'express'
 const router = Router()
 
 router.get('/reviews', (req, res) => {
-  res.send([
+  res.json([
     { id: 1, reviewTitle: 'Awesome location' },
     { id: 2, reviewTItle: 'Great Stay' }
   ])
 })
 
 router.get('/reviews/1', (req, res) => {
-  res.send({ id: 1, reviewTitle: 'Awesome location' })
+  res.json({ id: 1, reviewTitle: 'Awesome location' })
 })
 
 export default router
