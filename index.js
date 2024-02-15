f8db4c38e58af76abebd0c77de0fa9f028344b42
PhilJG <66040850+PhilJG@users.noreply.github.com>
2024-02-15 13:14:44 +0700

    create usersRoutes

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 import express from 'express'
 const app = express()
-app.listen(4100, () => { 
-    console.log('Airbnb API ready on localhost:4100')
+
+import usersRouter from './routes/usersRouter.js'
+
+app.use(usersRouter)
+
+app.listen(4100, () => {
+  console.log('Airbnb API ready on localhost:4100')
 })
