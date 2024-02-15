16f3177a52e06b57394285affcf745c5505cb2ae
ValeriaSml <114675105+ValeriaSml@users.noreply.github.com>
2024-02-15 13:26:44 +0700

    update send to json

--- routes/bookingsRoute.js
+++ routes/bookingsRoute.js
@@ -2,14 +2,14 @@ import { Router } from 'express'
 const router = Router()
 
 router.get('/bookings', (req, res) => {
-  res.send([
+  res.json([
     { id: 1, bookingPrice: 200 },
     { id: 2, bookingPrice: 500 }
   ])
 })
 
 router.get('/bookings/1', (req, res) => {
-  res.send({ id: 1, reviewbookingPrice: 200 })
+  res.json({ id: 1, reviewbookingPrice: 200 })
 })
 
 export default router
