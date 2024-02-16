ca5f96c78e1481420ab2640e5554a4eeb8fb2ac0
SlviACH6 <146087759+SlviACH6@users.noreply.github.com>
2024-02-16 08:45:32 +0700

    changes on photos-routes

--- routes/photosRoutes.js
+++ routes/photosRoutes.js
@@ -3,14 +3,14 @@ import { Router } from 'express'
 const router = Router()
 
 router.get('/photos', (req, res) => {
-  res.send([
+  res.json([
     { id: 1, white_house: '' },
     { id: 2, red_house: '' }
   ])
 })
 
 router.get('/photos/1', (req, res) => {
-  res.send({ id: 1, white_house: '' })
+  res.json({ id: 1, white_house: '' })
 })
 
 export default router
