46bdf6f07bada232ea89bfa83422aa82bf0927e2
PhilJG <66040850+PhilJG@users.noreply.github.com>
2024-02-16 12:33:48 +0700

    Update reviewsRoutes.js

--- routes/reviewsRoutes.js
+++ routes/reviewsRoutes.js
@@ -5,7 +5,7 @@ const router = Router()
 
 router.get('/reviews', async (req, res) => {
   try {
-    const { rows } = db.query(`SELECT * FROM reviews`)
+    const { rows } = await db.query(`SELECT * FROM reviews`)
     console.log(rows)
     res.json(rows)
   } catch (err) {
@@ -14,8 +14,19 @@ router.get('/reviews', async (req, res) => {
   }
 })
 
-router.get('/reviews/1', (req, res) => {
-  res.json({ id: 1, reviewTitle: 'Awesome location' })
+let reviewId = 2
+
+router.get(`/reviews/${reviewId}`, async (req, res) => {
+  try {
+    const { rows } = await db.query(
+      `SELECT * FROM reviews WHERE review_id = ${reviewId}`
+    )
+    res.json(rows)
+    console.log(rows)
+  } catch (err) {
+    console.error(err.message)
+    res.json({ error: err.message })
+  }
 })
 
 export default router
