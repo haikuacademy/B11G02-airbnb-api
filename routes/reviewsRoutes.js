d45d6234a97e66b903427fdbe6c0cd5414c2e471
PhilJG <66040850+PhilJG@users.noreply.github.com>
2024-02-16 12:24:04 +0700

    complete select all query

--- routes/reviewsRoutes.js
+++ routes/reviewsRoutes.js
@@ -1,11 +1,17 @@
 import { Router } from 'express'
+import db from '../db.js'
+
 const router = Router()
 
-router.get('/reviews', (req, res) => {
-  res.json([
-    { id: 1, reviewTitle: 'Awesome location' },
-    { id: 2, reviewTItle: 'Great Stay' }
-  ])
+router.get('/reviews', async (req, res) => {
+  try {
+    const { rows } = db.query(`SELECT * FROM reviews`)
+    console.log(rows)
+    res.json(rows)
+  } catch (err) {
+    console.error(err.message)
+    res.json({ error: err.message })
+  }
 })
 
 router.get('/reviews/1', (req, res) => {
