bac0b9567bc17312e2aa04a1f5b76c3b8ba2c6b6
PhilJG <66040850+PhilJG@users.noreply.github.com>
2024-02-19 15:07:30 +0700

    Update housesRoutes.js

--- routes/housesRoutes.js
+++ routes/housesRoutes.js
@@ -6,13 +6,31 @@ const router = Router()
 // this route gets all the houses
 
 router.get('/houses', async (req, res) => {
+  let queryString = ''
   try {
-    const { rows } = await db.query(`SELECT * FROM houses`)
-    res.json(rows)
+    if (req.query.location) {
+      queryString += req.query.location
+    }
+    if (!queryString) {
+      const { rows } = await db.query(`SELECT * FROM houses`)
+      res.json(rows)
+    } else {
+      const { rows } = await db.query(
+        `SELECT * FROM houses WHERE location = '${queryString}'`
+      )
+      res.json(rows)
+    }
   } catch (err) {
     console.log(err.message)
     res.json({ error: err.message })
   }
+})
+router.get('/houses', async (req, res) => {
+  const { rows } = await db.query(
+    `SELECT * FROM houses WHERE location = ${location}`
+  )
+  console.log(rows)
+  res.json(rows)
 })
 
 // this route gets a specific house ID based on the route parameter
