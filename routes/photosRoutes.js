49e5657f5ab9b0247fe5325639d6a24a8a4f103b
PhilJG <66040850+PhilJG@users.noreply.github.com>
2024-02-21 11:21:26 +0700

    complete photo routes

--- routes/photosRoutes.js
+++ routes/photosRoutes.js
@@ -4,9 +4,23 @@ import db from '../db.js'
 const router = Router()
 
 router.get('/photos', async (req, res) => {
+  let houseId = req.query.house
+
+  let queryString = `SELECT * FROM houses_pictures WHERE house_id = ${houseId}`
+
+  if (houseId) {
+    queryString += ``
+  }
+
   try {
-    const { rows } = await db.query('SELECT * FROM houses_pictures')
-    console.log(rows)
+    const { rows } = await db.query(queryString)
+
+    console.log(`rows.length ${rows.length}`)
+
+    if (!rows.length) {
+      throw new Error('house parameter is required')
+    }
+
     res.json(rows)
   } catch (err) {
     console.error(err.message)
