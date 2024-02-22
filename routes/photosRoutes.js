55671e8f0bfa5e1f5e027557235007c9b48b0b8a
SlviACH6 <146087759+SlviACH6@users.noreply.github.com>
2024-02-22 10:49:17 +0700

    PATCH photos route

--- routes/photosRoutes.js
+++ routes/photosRoutes.js
@@ -3,6 +3,28 @@ import db from '../db.js'
 
 const router = Router()
 
+<<<<<<< Updated upstream
+=======
+//POST PHOTOS ROUTE
+router.post('/photos', async (req, res) => {
+  const { photo_url, house_id } = req.body
+  console.log(req.body)
+
+  const newPhotoQuery = `INSERT INTO houses_pictures (photo_url, house_id)
+      VALUES ('${photo_url}', ${house_id})
+      RETURNING * `
+  console.log(newPhotoQuery)
+  try {
+    const { rows } = await db.query(newPhotoQuery)
+    res.json(rows)
+  } catch (err) {
+    console.error(err.message)
+    res.json({ error: err.message })
+  }
+})
+
+// GET PHOTOS ROUTES
+>>>>>>> Stashed changes
 router.get('/photos', async (req, res) => {
   let houseId = req.query.house
 
@@ -37,6 +59,29 @@ router.get('/photos/:photoId', async (req, res) => {
     if (!rows.length) {
       throw new Error(`The photo Id number ${photoId} does not exist.`)
     }
+    console.log(rows)
+    res.json(rows)
+  } catch (err) {
+    console.error(err.message)
+    res.json({ error: err.message })
+  }
+})
+
+////PATCH PHOTOS ID ROUTE
+router.patch('/photos/:photoId', async (req, res) => {
+  let photoIdPatch = req.params.photoId
+  let photoUrl = req.body.photo_url
+
+  try {
+    const { rows } = await db.query(`UPDATE houses_pictures
+      SET photo_url = '${photoUrl}'
+      WHERE photo_id = ${photoIdPatch} 
+      RETURNING photo_url `)
+
+    if (!rows.length) {
+      throw new Error('The house ID provided is not valid')
+    }
+
     console.log(rows)
     res.json(rows)
   } catch (err) {
